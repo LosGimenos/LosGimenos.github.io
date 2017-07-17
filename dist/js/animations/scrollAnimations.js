edd33667f2d7071491a95aedd64028f2c4b0e9e4
Larry Hernandez <larryhernanjr@gmail.com>
2017-07-17 17:31:09 -0400

    allow scrolling entry for web list items

--- dist/js/animations/scrollAnimations.js
+++ dist/js/animations/scrollAnimations.js
@@ -1,1 +1,11 @@
-scrollAnimations.js
+window.onload = () => {
+  const controller = new ScrollMagic.Controller();
+
+  document.querySelectorAll('web-project-item').forEach((item) => {
+    const projectItemScene = new ScrollMagic.Scene({
+      triggerElement: item,
+    })
+      .setClassToggle(item, 'swipe')
+      .addTo(controller);
+  });
+};
